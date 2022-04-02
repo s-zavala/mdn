7d51ed3c6a96104b7b41696349ceff94b75ee69b
Sofia Zavala <szavala3@outlook.com>
2022-04-02 16:56:28 -0400

    main js fixed evil circle bug

--- javascript/objects/bb/m.js
+++ javascript/objects/bb/m.js
@@ -31,33 +31,33 @@ class Shape {
 
 class EvilBall extends Shape {
   constructor(x, y) {
-      super(x, y, 20, 20);
-      this.color = "white";
-      this.size = 10;
-      window.addEventListener('keydown', (e) => {
-          switch(e.key) {
-            case 'a':
-              this.x -= this.velX;
-              break;
-            case 'd':
-              this.x += this.velX;
-              break;
-            case 'w':
-              this.y -= this.velY;
-              break;
-            case 's':
-              this.y += this.velY;
-              break;
-          }
-        });
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+    window.addEventListener('keydown', (e) => {
+      switch(e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    });
   }
 
   draw() {
-      ctx.beginPath();
-      ctx.lineWidth = 5;
-      ctx.strokeStyle = this.color;
-      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-      ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
     }
 
   checkBounds() {
@@ -167,7 +167,7 @@ balls.push(ball);
 }
 
 // draw evil circle
-const evilCircle = new EvilBall(Math.floor(width/2), Math.floor(height));
+const evilCircle = new EvilBall(100, 100);
 
 // animate
 function loop() {
@@ -181,7 +181,11 @@ function loop() {
           ball.collisionDetect();      
       }   
   }
-   
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+
   requestAnimationFrame(loop);
  }
  
