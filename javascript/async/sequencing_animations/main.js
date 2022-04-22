f935d573ed5b28493f86afcb22115093e979ab3f
Sofia Zavala <szavala3@outlook.com>
2022-04-22 19:31:45 -0400

    js async promise chain

--- javascript/async/sequencing_animations/main.js
+++ javascript/async/sequencing_animations/main.js
@@ -13,29 +13,29 @@ const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-function animeate1(init, callback) {
-  init.animate(aliceTumbling, aliceTiming);
-  callback(0);
+// Promise Chain
+
+function anim_alice(init) {
+  const animation = init.animate(aliceTumbling, aliceTiming);
+  return animation.finished
 }
 
-function animate2(init, callback) {
-  init.animate(aliceTumbling, aliceTiming);
-  callback(0);
+// anim_alice(alice1)
+//   .then(result => anim_alice(alice2))
+//   .then(result => anim_alice(alice3))
+//   .then(finalResult => console.log("Hello Alice"));
+
+// Using async and await
+
+async function sequence() {
+  try {
+    const result = await anim_alice(alice1);
+    const newResult = await anim_alice(alice2);
+    const finalResult = await anim_alice(alice3);
+  }
+  catch(error) {
+    failureCallback(error)
+  }
 }
 
-function animate3(init, callback) {
-  init.animate(aliceTumbling, aliceTiming);
-  callback(0)
-}
-
-function doOperation() {
-  animeate1(alice1, nul => {
-    animate2(alice2, nul => {
-      animate3(alice3, nul => {
-        console.log("Hello world.")
-      });
-    });
-  });
-};
-
-doOperation();
+sequence()
